e9726ff6c3f06afbc2181e9e854ad0695c500823
BekirPiralp <bpir33@gmail.com>
2022-04-28 00:32:49 +0300

    daire tanımlama nesne ayar

--- lib/Servisler/DaireServis.dart
+++ lib/Servisler/DaireServis.dart
@@ -46,7 +46,7 @@ class DaireServisi {
       http.Response response = await http.post(
           WebServisConnection.UrlDaireTanimlaSno(queryparams),
           headers: WebServisConnection.baslik,
-          body: daireSakini.cevirNesnedenJsonMap().toString());//nesne yollama
+          body: daireSakini.cevirNesnedenJsonMap());//nesne yollama
 
       if (response.statusCode == ResponseKod.basarili && response.body.toUpperCase().compareTo("NULL")!=0) {
         result = true;
